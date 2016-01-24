53e120c7bfd58a9de363430c766a44af5cdd8fa8
Jolita Grazyte <jolita.grazyte@student.kdg.be>
2016-01-24 16:35:46 +0100

    updatded app style, updated locker status

--- code/lockedornot_app/www/js/pusher.js
+++ code/lockedornot_app/www/js/pusher.js
@@ -1,52 +1,45 @@
-//                 Enable pusher logging - don't include this in production
-//                Pusher.log = function(message) {
-//                    if (window.console && window.console.log) {
-//                        window.console.log(message);
-//                    }
-//                };
-//alert('boom');
 
 var pusher = new Pusher('dc3afc8d4e52c7fe10c9', {
     encrypted: true
 });
 
-
-pusher.connection.bind( 'error', function( err ) {
-    if( err.data.code === 404 ) {
-        console.log('>>> detected limit error');
-    }
-});
-
-pusher.connection.bind('connected', function() {
-    $('div#status').text('Real time is go!');
-});
-
-//var device_channel = pusher.subscribe('device');
-//device_channel.bind('device-state', function(data) {
-//    //console.log(data.text);
 //
-//
-//
+//pusher.connection.bind( 'error', function( err ) {
+//    if( err.data.code === 404 ) {
+//        console.log('>>> detected limit error');
+//    }
 //});
+//
+//pusher.connection.bind('connected', function() {
+//    $('div#status').text('Real time is go!');
+//});
+
+
 var msg_channel = pusher.subscribe('notifications');
     msg_channel.bind('new-notification', function(data){
 
         var message = data.text;
+
+        //console.log(data);
         $('.notification').text(message);
 
 
-        var message = data.device_state;
-        //$('div.notification').text(message);
+        console.log(message);
 
-        var el = document.getElementById('ion-content');
-        if(message === '1'){
+        var device_state = data.device_state;
+        var lock = $('.lock');
+        if(device_state == 1){
+            lock.removeClass('unlocked');
+            lock.addClass('locked');
 
-            el.style.background = '#50bc57';
         }
         else{
+            lock.removeClass('locked');
+            lock.addClass('unlocked');
 
-            el.style.background = '#fb5542';
         }
+
+
 
     });
 
