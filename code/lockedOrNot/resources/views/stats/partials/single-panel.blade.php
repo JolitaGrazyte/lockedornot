79a549dbe9320d1343e478c8906e8a0fbce9f6ca
root <jonasvanreeth@gmail.com>
2016-08-11 12:02:52 +0200

    prep aws  & status boxes gone

--- code/lockedOrNot/resources/views/stats/partials/single-panel.blade.php
+++ code/lockedOrNot/resources/views/stats/partials/single-panel.blade.php
@@ -1,9 +1,6 @@
 <div class="col-lg-3 col-sm-6 rates-panel">
     <div class="panel panel-{{ $panel['color'] }} {{ substr($panel['stats'], 0, 7) }}"
          data-toggle="tooltip"  title="{{ $panel['title'] }}">
-                    <span class="tooltip">
-                        {{ $panel['title'] }}
-                    </span>
         <div class="panel-heading">
             <div class="row">
                 <div class="col-xs-12 text-right">
